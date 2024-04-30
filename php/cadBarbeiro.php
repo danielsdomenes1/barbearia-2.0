68f68c3cf412ac7287f5450dd73b93137035684a
DANIEL SANCHES DOMENES <daniel.sdomenes1@senacsp.edu.br>
2024-04-30 17:28:38 -0300

    iniciando mais um trabalho de api, com MYSQL

---
+++ php/cadBarbeiro.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['nome'])&&isset($_POST['senha'])&& $_POST['email']&& $_POST['cpf']&&$_POST['telefone']&&$_POST['aniversario']&& $_POST['data']){
+   
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $telefone = $_POST['telefone'];
+    $aniversario = $_POST['aniversario'];
+    $dataInicio = $_POST['data'];
+
+}
+
+
+
+?>
